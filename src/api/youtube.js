2cc18464a2c4909cc03068a3d64372874b5985e8
fahmi-arff <fahmi.arff@gmail.com>
2019-09-05 13:42:44 +0700

    retrieve data from youtube api

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -3,11 +3,17 @@ import axios from 'axios';
 const KEY = 'AIzaSyC9LtqU9zeuT6hzCtrjRU0V5WikeRvsPFk';
 
 export default axios.create({
-  baseURL: 'https://www.googleapis.com/youtube/v3';
+  baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
     part: 'snippet',
-    maxResult: 5,
+    maxResults: 5,
     key: KEY
   }
 })
 
+// export default axios.create({
+//   baseURL: 'https://api.unsplash.com',
+//   headers: {
+//     Authorization: 'Client-ID c59e14f9eb75a1ccba65ac8c6c564af19299c8cd474cae34566789061532f5a5'
+//   }
+// });
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../api/youtube'
 
 class App extends React.Component {
+  onTermSubmit = term => {
+    youtube.get('/search', {
+      params: {
+        q: term
+      }
+    })
+    // youtube.get('/search/photos', {
+    //   params: { query: term },
+    // });
+  }
+
   render() {
     return(
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
       </div>
     ) 
   }
