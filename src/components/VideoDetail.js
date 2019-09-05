bc26eed1f051e0f1ebf6a8c62a04e1869dbced19
fahmi-arff <fahmi.arff@gmail.com>
2019-09-05 15:36:29 +0700

    displaying a video player

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -4,8 +4,16 @@ const VideoDetai = ({ video }) => {
   if(!video) {
     return <div>Loading ...</div>
   }
+
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
+
   return (
     <div>
+      <div>
+      <div className="ui embed">
+        <iframe src={videoSrc} />
+      </div>
+    </div>
       <div className="ui segment">
         <h4 className="ui header">{video.snippet.title}</h4>
         <p>{video.snippet.description}</p>
